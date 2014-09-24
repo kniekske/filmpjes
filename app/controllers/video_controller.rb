5dd2e18420fe0dc758822c4ec5032216f2a42391
Kniekske <moniekverlaek@gmail.com>
2014-09-24 10:59:43 +0800

    form not working

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -9,15 +9,11 @@ class VideoController < ApplicationController
    end
 
    def create
-
    	puts params["video"]
-
-   	puts "Videos Create"
+   	puts "Create"
    	@video = Video.new
-
    	@video.title = params["video"]["title"]
    	@video.link = params["video"]["link"]
-
 
    	if @videos.save
    			redirect_to video_path(@videos)
@@ -26,6 +22,27 @@ class VideoController < ApplicationController
    		end
    	puts @videos
 	end
+
+
+ def update
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to @video, notice: 'Update' }
+        format.json { render :show, status: :ok, location: @video }
+      else
+        format.html { render :edit }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @video.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Destroy!' }
+      format.json { head :no_content }
+    end
+  end
 
 
 end
