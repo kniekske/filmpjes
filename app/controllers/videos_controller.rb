27e3fcf9c49b8d565fe3cdbc8d35b1b052b9c816
Kniekske <moniekverlaek@gmail.com>
2014-09-25 09:51:42 +0800

    saving to db

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,20 +8,38 @@ class VideosController < ApplicationController
    		@video = Video.new
    end
 
-   def create
-   	puts params["video"]
-   	puts "Create"
-   	@video = Video.new
-   	@video.title = params["video"]["title"]
-   	@video.link = params["video"]["link"]
+#   def create
+#   	puts params["video"]
+#   	puts "Create"
+#   	@video = Video.new
+#   	@video.title = params["video"]["title"]
+#  	@video.link = params["video"]["link"]
 
-   	if @videos.save
-   			redirect_to video_path(@videos)
-   		else
-   			render :action => :create
-   		end
-   	puts @videos
-	end
+#   	if @videos.save
+#   			redirect_to video_path(@videos)
+#   		else
+#   			render :action => :create
+#   		end
+#   	puts @videos
+#end
+
+def show
+	@video = Video.find(params[:id])
+end	
+
+  def create
+    @video = Video.new(video_params)
+
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: 'Video was successfully created.' }
+        format.json { render :show, status: :created, location: @video }
+      else
+        format.html { render :new }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 
  def update
@@ -45,5 +63,9 @@ class VideosController < ApplicationController
   end
 
 
+  private
+  def video_params
+  	 params.require(:video).permit(:title, :link)
+  end
 end
 
